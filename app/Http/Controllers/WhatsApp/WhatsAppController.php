6474addbbb1c5553610edaf120b5e7cda1c2775c
Septian <septian.wijaya@smart-it.co.id>
2024-09-23 21:32:06 +0700

    integrasi wa watsap web

--- app/Http/Controllers/WhatsApp/WhatsAppController.php
+++ app/Http/Controllers/WhatsApp/WhatsAppController.php
@@ -22,7 +22,7 @@ class WhatsAppController extends Controller
             // Ganti '0' di awal nomor dengan '62'
             $number = substr_replace($number, '62', 0, 1);
         }
-        
+
         return $number;
     }
 
@@ -59,14 +59,35 @@ class WhatsAppController extends Controller
 
         $telepon = $this->convertPhoneNumber($request->number);
 
-        $client = new Client();
-        $apiUrl = config('services.api.whatsapp_url');
-        $response = $client->post($apiUrl.'send-message', [
-            'json' => [
-                'number' => $telepon,
-                'message'=> $message
-            ],
-        ]);
+        $api_key   = 'e7b82e6e0668b27b30ac626ed95be856f88ce34a';
+        $id_device = '12345';
+        $url   = 'https://api.watsap.id/send-message';
+        $no_hp = $telepon;
+        $pesan = $message;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 0); // batas waktu response
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_POST, 1);
+
+        $data_post = [
+        'id_device' => $id_device,
+        'api-key' => $api_key,
+        'no_hp'   => $no_hp,
+        'pesan'   => $pesan
+        ];
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data_post));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        dd($response);
 
         $responseBody = json_decode($response->getBody(), true);
 
