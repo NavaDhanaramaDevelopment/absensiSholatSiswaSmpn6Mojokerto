36cfb1af9c2d1f0e95097124d71f610c329756dd
septianwijaya45 <septianwijaya170845@gmail.com>
2024-10-07 09:13:08 +0700

    update migration kelas

--- database/migrations/2024_10_06_195224_create_kelas_table.php
+++ database/migrations/2024_10_06_195224_create_kelas_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('m_classes', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kelas');
-            $table->foreignId('teacher_id')->references('id')->on('m_teachers');
             $table->timestamps();
         });
     }
